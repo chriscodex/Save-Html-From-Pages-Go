b98572c9d6eca4725e356a234e07daf79ba90ab2
Christian <3003html@gmail.com>
2022-06-12 22:32:33 -0500

    Struct writer web created and his functions write

--- main.go
+++ main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
+
+type writerWeb struct{}
+
+func (writerWeb) Write(p []byte) (int, error) {
+	fmt.Println(p)
+	return 0, nil
+}
 
 func main() {
 	response, err := http.Get("http://google.com")
 	if err != nil {
-		fmt.Println(response)
+		fmt.Println(err)
 	}
+	e := writerWeb{}
+	io.Copy(e, response.Body)
 }
